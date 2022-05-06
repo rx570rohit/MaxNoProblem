9210695473a5d7f321151669601d4c415e183dcf
rx570rohit <rohit.soni2018@vitbhopal.ac.in>
2022-05-06 18:18:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNoProblem
+{
+    internal class Program
+    {
+        public static void Main(String [] arg)
+        {
+            Console.WriteLine(Maxint.MaximumIntegerNumber(1,5,6));
+        }
+    }
+}
